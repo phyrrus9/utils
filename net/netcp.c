1cdacfc131a71224ccc4c0c83c17592e4c9e43b5
Ethan Laur <phyrrus9@gmail.com>
2013-09-11 09:33:11 -0400

    Added netcp.c netcp.c: My implementation of airdrop, unsandboxed

---
+++ net/netcp.c
@@ -0,0 +1,11 @@
+/* network copy by phyrrus9 © 2013 */
+
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int sendFile(const char * filename, const char * ip_addr)
+{
+    int sockfd; //file for our socket
+}
